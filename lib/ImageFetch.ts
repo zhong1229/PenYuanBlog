049e81bd9677c72f1303aa4db0405857bd6de955
zhong1229 <3052288628@qq.com>
2024-03-03 14:47:50 +0800

    ok

--- lib/ImageFetch.ts
+++ lib/ImageFetch.ts
@@ -1,7 +1,7 @@
 import { HttpUrl } from "@/config";
 
 export async function findImageList() {
-  const response = await fetch(HttpUrl + `/media`);
+  const response = await fetch(HttpUrl + `/media`, { cache: "no-cache" });
   const ImageList = await response.json();
 
   return [...ImageList];
